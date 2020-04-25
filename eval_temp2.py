656f50d7dee1ef3ac5b0869de1d4c5cad36b9bf8
Welcome to Heather's GitHub page <37559580+HeatherJiaZG@users.noreply.github.com>
2020-04-25 17:00:52 -0500

    Delete eval_temp2.py

--- eval_temp2.py
+++
@@ -1,9 +0,0 @@
-            
-
-            # Visualize the matches.
-            color = cm.jet(mconf)
-            
-            make_matching_plot(
-                image0, image1, kpts0, kpts1, mkpts0, mkpts1, color,
-                text = [], viz_path, opt.show_keypoints,
-                opt.fast_viz, opt.opencv_display, 'Matches', small_text=[])
